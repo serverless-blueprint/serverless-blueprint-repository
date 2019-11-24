20b4d7fac641bd72d657f2e45e9dbc22deb50e31
sarthakmakhija <sarthak.makhija@gmail.com>
2019-11-25 00:18:22 +0530

    Sarthak | Adds support for generating findById method

--- src/org/blueprint/serverless/repository/model/DynamoDbRepositoryFeatures.ts
+++ src/org/blueprint/serverless/repository/model/DynamoDbRepositoryFeatures.ts
@@ -2,17 +2,31 @@ export class DynamoDbRepositoryFeatures {
     constructor(public readonly className: string,
                 public readonly tableName: string,
                 public readonly region: string,
-                public readonly findAllMethodName: string) {
+                public readonly findAllMethodSupported: boolean,
+                public readonly findAllMethodName: string,
+                public readonly findByIdMethodSupported: boolean,
+                public readonly findByIdMethodName: string) {
     }
 
     static builder(className: string, tableName: string) {
         return new DynamoDbRepositoryFeaturesBuilder(className, tableName)
+    }
+
+    isFindAllRequired(): boolean {
+        return this.findAllMethodSupported;
+    }
+
+    isFindByIdRequired(): boolean {
+        return this.findByIdMethodSupported;
     }
 }
 
 class DynamoDbRepositoryFeaturesBuilder {
     private region: string = "us-east-1";
     private findAllMethodName: string = "findAll";
+    private findByIdMethodName: string = "findById";
+    private findAllMethodSupported: boolean = false;
+    private findByIdMethodSupported: boolean = false;
 
     constructor(private className: string, private tableName: string) {
     }
@@ -22,13 +36,34 @@ class DynamoDbRepositoryFeaturesBuilder {
         return this;
     }
 
+    supportFindAllMethod() {
+        this.findAllMethodSupported = true;
+        return this;
+    }
+
+    supportFindByIdMethod() {
+        this.findByIdMethodSupported = true;
+        return this;
+    }
+
     withFindAllMethodName(name: string) {
         this.findAllMethodName = name;
         return this;
     }
 
+    withFindByIdMethodName(name: string) {
+        this.findByIdMethodName = name;
+        return this;
+    }
+
     build() {
-        return new DynamoDbRepositoryFeatures(this.className,
-            this.tableName, this.region, this.findAllMethodName);
+        return new DynamoDbRepositoryFeatures(
+            this.className,
+            this.tableName,
+            this.region,
+            this.findAllMethodSupported,
+            this.findAllMethodName,
+            this.findByIdMethodSupported,
+            this.findByIdMethodName);
     }
 }
--- test/org/blueprint/serverless/repository/synthesize/DynamoDbRepositorySynthesizer.integration.spec.ts
+++ test/org/blueprint/serverless/repository/synthesize/DynamoDbRepositorySynthesizer.integration.spec.ts
@@ -6,7 +6,7 @@ import {DynamoDbRepositoryFeatures} from "../../../../../../src/org/blueprint/se
 
 describe('DynamoDb Repository Synthesizer (Integration Test)', () => {
 
-    const expectedCode = `
+    const codeWithFindAll = `
 const dynamoDbClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
 class ServerlessRepository {
 
@@ -21,11 +21,45 @@ class ServerlessRepository {
 
 module.exports = ServerlessRepository;`.trim();
 
-    it('should return dynamo db repository code', function () {
+    const codeWithFindById = `
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+class ServerlessRepository {
+
+    async findById(id) {
+        const request = {
+            TableName: "serverless-table",
+            Key: {
+                "id": id
+            }
+        };
+        const promise = await dynamoDbClient.get(request).promise();
+        return promise.Items;
+    }
+}
+
+module.exports = ServerlessRepository;`.trim();
+
+    it('should return dynamo db repository code with find all method', function () {
         let dynamoDbRepositorySynthesizer = new DynamoDbRepositorySynthesizer();
-        let dynamoDbRepositoryFeatures = DynamoDbRepositoryFeatures.builder("ServerlessRepository", "serverless-table").withFindAllMethodName( "findAll").build();
+        let dynamoDbRepositoryFeatures = DynamoDbRepositoryFeatures
+            .builder("ServerlessRepository", "serverless-table")
+            .supportFindAllMethod()
+            .withFindAllMethodName( "findAll").build();
+
         let repositoryCode = dynamoDbRepositorySynthesizer.synthesize(dynamoDbRepositoryFeatures);
 
-        expect(repositoryCode).to.equal(expectedCode)
+        expect(repositoryCode).to.equal(codeWithFindAll)
+    });
+
+    it('should return dynamo db repository code with find by id method', function () {
+        let dynamoDbRepositorySynthesizer = new DynamoDbRepositorySynthesizer();
+        let dynamoDbRepositoryFeatures = DynamoDbRepositoryFeatures
+            .builder("ServerlessRepository", "serverless-table")
+            .supportFindByIdMethod()
+            .withFindByIdMethodName( "findById").build();
+
+        let repositoryCode = dynamoDbRepositorySynthesizer.synthesize(dynamoDbRepositoryFeatures);
+
+        expect(repositoryCode).to.equal(codeWithFindById)
     });
 });
