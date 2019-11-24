2875aa00cbdcea66b2c5aaec0c73798629504db9
sarthakmakhija <sarthak.makhija@gmail.com>
2019-11-24 18:00:38 +0530

    Sarthak | Adds a test to render a repository class globally initialized dynamo db client

--- test/org/blueprint/serverless/repository/DynamoDbRepositoryRenderer.spec.ts
+++ test/org/blueprint/serverless/repository/DynamoDbRepositoryRenderer.spec.ts
@@ -3,12 +3,13 @@ import 'mocha';
 
 class DynamoDbRepositoryRenderer {
     render(dynamoDbRepositoryFeatures: DynamoDbRepositoryFeatures): string {
-        return `class ${dynamoDbRepositoryFeatures.className}`;
+        return `const dynamo = new AWS.DynamoDB.DocumentClient({region: '${dynamoDbRepositoryFeatures.region}'});`
+        + `class ${dynamoDbRepositoryFeatures.className}`;
     }
 }
 
 class DynamoDbRepositoryFeatures {
-    constructor(public readonly className: string) {
+    constructor(public readonly className: string, public readonly region: string) {
     }
 }
 
@@ -16,9 +17,17 @@ describe('DynamoDb Repository Renderer', () => {
 
     it('should return dynamo db repository with specified class name', () => {
         let dynamoDbRepositoryRenderer = new DynamoDbRepositoryRenderer();
-        let dynamoDbRepositoryFeatures = new DynamoDbRepositoryFeatures("ServerlessRepository");
+        let dynamoDbRepositoryFeatures = new DynamoDbRepositoryFeatures("ServerlessRepository", "ap-south-1");
         let repositoryCode = dynamoDbRepositoryRenderer.render(dynamoDbRepositoryFeatures);
 
-        expect(repositoryCode).to.contains("class ServerlessRepository");
+        expect(repositoryCode).to.contains("class ServerlessRepository", "");
+    });
+
+    it('should return dynamo db repository dynamo db client initialized globally', () => {
+        let dynamoDbRepositoryRenderer = new DynamoDbRepositoryRenderer();
+        let dynamoDbRepositoryFeatures = new DynamoDbRepositoryFeatures("", "ap-south-1");
+        let repositoryCode = dynamoDbRepositoryRenderer.render(dynamoDbRepositoryFeatures);
+
+        expect(repositoryCode).to.contains("const dynamo = new AWS.DynamoDB.DocumentClient({region: 'ap-south-1'});");
     });
 });
